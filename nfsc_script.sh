192c118dd998f2e593303631ac290c166d460e69
molchanovar <64701071+molchanovar@users.noreply.github.com>
2020-12-22 19:39:54 +0300

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -2,4 +2,7 @@
 
 yum install nfs-utils
 mkdir -p /mnt/nfs/upload
+
+# Монтирование текущее + после перезагрузки в fstab
 mount -t nfs 192.168.50.10:/var/NFSshare /mnt/nfs/upload
+echo "192.168.50.10:/var/NFSshare /mnt/nfs//mnt/nfs/upload nfs rw,hard,intr,rsize=8192,wsize=8192,timeo=14 0 0" >> /etc/fstab

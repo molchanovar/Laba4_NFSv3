ee5a124468fd19adb3c54f94e5b298652eed90f8
molchanovar <64701071+molchanovar@users.noreply.github.com>
2020-12-24 14:59:51 +0300

    Update nfss_script.sh

--- nfss_script.sh
+++ nfss_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Установка пакетов для работы с NFS, создание общей папки и запуск сервисов 
+# Установка пакетов для работы с NFS, создание общей папки и запуск сервисов + права user'у nfs
 yum install nfs-utils
-mkdir /var/nfsshare
+mkdir -p /var/NFSshare
+chown -R nobody: /var/NFSshare
 systemctl enable {rpcbind,nfs-server,nfs-lock,nfs-idmap}
 systemctl restart {rpcbind,nfs-server,nfs-lock,nfs-idmap}
 
 # Запись в export файл с указанием прав
-echo "192.168.50.11(rw,sync)" >> /etc/exports
+echo "/var/NFSshare 192.168.50.0/24(rw,sync,no_all_squash,root_squash)" >> /etc/exports
 systemctl restart nfs-server
 
 # Настройка файрволла на работу с NFS
@@ -15,3 +16,4 @@ firewall-cmd --permanent --zone=public --add-service=nfs
 firewall-cmd --permanent --zone=public --add-service=mountd
 firewall-cmd --permanent --zone=public --add-service=rpc-bind
 firewall-cmd --reload
+a

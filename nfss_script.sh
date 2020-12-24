0cbdb2234e9aba8d7598e2cb60b0de03063dd4b5
molchanovar <64701071+molchanovar@users.noreply.github.com>
2020-12-24 15:00:01 +0300

    Update nfss_script.sh

--- nfss_script.sh
+++ nfss_script.sh
@@ -16,4 +16,3 @@ firewall-cmd --permanent --zone=public --add-service=nfs
 firewall-cmd --permanent --zone=public --add-service=mountd
 firewall-cmd --permanent --zone=public --add-service=rpc-bind
 firewall-cmd --reload
-a
